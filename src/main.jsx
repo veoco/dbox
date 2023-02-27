3b2091acf28b361f3322733ea485ec973d507e64
veoco <one@nomox.cn>
2023-02-27 20:06:58 +0800

    新增 swr 包

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { SWRConfig } from 'swr'
+
 import App from './app'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('app')).render(
   <React.StrictMode>
-    <App />
+    <SWRConfig
+      value={{
+        fetcher: (resource, init) => fetch(resource, init).then(res => res.json())
+      }}
+    >
+      <App />
+    </SWRConfig>
   </React.StrictMode>,
 )
